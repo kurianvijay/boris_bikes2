507e9be3489ae95c8802cf2b704ea77d73d7db12
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-09-18 15:52:09 +0100

    Completed step 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,5 +4,12 @@ describe DockingStation do
   it 'releases a bike' do
     expect(subject).to respond_to :release_bike
   end
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
+
 
